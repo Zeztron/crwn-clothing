287a469b1df421a6db34f9e3efc5b5c31bfe7b85
Zeztron <pateltharsh@gmail.com>
2019-07-07 13:40:03 -0400

    changed directories around

---
+++ src/pages/homepage/Homepage.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Directory from '../../components/directory/Directory';
+import "./Homepage.scss";
+
+const Home = () => (
+  <div className="homepage">
+    <Directory/>
+  </div>
+);
+
+export default Home;
