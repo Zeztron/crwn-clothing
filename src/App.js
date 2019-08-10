eee3fcf6af0c86c572ee13c7df93054910ab09e2
Zeztron <pateltharsh@gmail.com>
2019-08-10 12:58:58 -0400

    added collection item and preview

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Home from './pages/homepage/Homepage';
+import ShopPage from './pages/shop/Shop';
 import './App.css'
 
 function App() {
   return (
     <div>
       <Switch>
-        <Route path='/' component={Home}/>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/shop" component={ShopPage} />
       </Switch>
     </div>
   );
